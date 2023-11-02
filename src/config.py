c96377a428625be10fee66c58bd6df69c624641a
d2gex <daniel.garcia@d2garcia.com>
2023-11-02 18:36:19 +0100

    Add first version of Motuclient working

--- src/config.py
+++ src/config.py
@@ -1,7 +1,13 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 file_path = Path(__file__).resolve()
 ROOT_PATH = file_path.parents[1]
-DATA_PATH = file_path.parents[3] / 'repos_data' / 'Points_habitat_time_position2.csv'
+DATA_PATH = file_path.parents[3] / 'repos_data' / 'afar_coper'
+dot_env = load_dotenv(ROOT_PATH / '.env')
+BASE_URL = 'http://my.cmems-du.eu/motu-web/Motu'
+COMPERNICUS_USERNAME = os.getenv('COMPERNICUS_USERNAME')
+COMPERNICUS_PASSWORD = os.getenv('COMPERNICUS_PASSWORD')
 
 
