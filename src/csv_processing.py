7d9b609cca36dd2c901f5baa55ea9c74f99f3757
d2gex <daniel.garcia@d2garcia.com>
2023-11-30 09:36:45 +0100

    Add depth field to the processing of csv

--- src/csv_processing.py
+++ src/csv_processing.py
@@ -28,12 +28,19 @@ class CsvProcessing:
             'date_max': max(data['time'])
         }
 
+    def _get_min_max_depth(self, data: pd.DataFrame) -> Dict[str, datetime]:
+        return {
+            'depth_min': min(data['depth']),
+            'depth_max': max(data['depth'])
+        }
+
     def get_max_area_per_dates(self) -> Dict[str, Dict]:
         df_by_years = self._split_csv_per_year(self.data)
         data = {}
         for year, df in df_by_years.items():
             coords = self._get_max_bounding_box(df)
             dates = self._get_min_max_date(df)
-            coords.update(dates)
+            depths = self._get_min_max_depth(df)
+            coords.update(**dates, **depths)
             data[year] = coords
         return data
