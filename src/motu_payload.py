063587f54197310458e5b11ced5b388915ab31c6
d2gex <daniel.garcia@d2garcia.com>
2023-11-30 09:42:19 +0100

    Refactor motu_payload.py to deal with list of dicts instead

--- src/motu_payload.py
+++ src/motu_payload.py
@@ -1,31 +1,30 @@
-import pandas as pd
 from typing import Dict, Any
 from datetime import datetime
 
 
 class MotuPayloadGenerator:
 
-    def __init__(self, data: pd.DataFrame, payload: Dict[str, Any], output_filename: str):
+    def __init__(self, data: Dict, payload: Dict[str, Any], output_filename: str):
         self.data = data
         self.payload = payload
         self.output_filename = output_filename
         self.area_details = []
 
-    def _process_row(self, row):
+    def _process_data_dict(self, year: str, data: Dict) -> Dict[str, Any]:
         tokens = self.output_filename.split(".")
         product_details = {
-            'latitude_min': row["latitude_min"],
-            'longitude_min': row["longitude_min"],
-            'latitude_max': row["latitude_max"],
-            'longitude_max': row["longitude_max"],
-            'depth_min': row["depth_min"],
-            'depth_max': row["depth_max"],
-            'out_name': f"{row['ID']}_{tokens[0]}.{tokens[-1]}",
-            'date_min': (datetime.strptime(row['date_min'], '%d/%m/%Y %H:%M:%S')).strftime('%Y-%m-%d %H:%M:%S'),
-            'date_max': (datetime.strptime(row['date_max'], '%d/%m/%Y %H:%M:%S')).strftime('%Y-%m-%d %H:%M:%S'),
+            'latitude_min': data["latitude_min"],
+            'longitude_min': data["longitude_min"],
+            'latitude_max': data["latitude_max"],
+            'longitude_max': data["longitude_max"],
+            'depth_min': data["depth_min"],
+            'depth_max': data["depth_max"],
+            'out_name': f"{year}_{tokens[0]}.{tokens[-1]}",
+            'date_min': data['date_min'].strftime('%Y-%m-%d %H:%M:%S'),
+            'date_max': data['date_max'].strftime('%Y-%m-%d %H:%M:%S'),
         }
         product_details.update(self.payload)
         return product_details
 
     def run(self):
-        return {row['ID']: self._process_row(row) for _, row in self.data.iterrows()}
+        return {year: self._process_data_dict(year, data) for year, data in self.data.items()}
