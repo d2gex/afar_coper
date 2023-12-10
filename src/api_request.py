f0f919aea51b842465315f951c8de9583248dba9
d2gex <daniel.garcia@d2garcia.com>
2023-12-10 10:29:17 +0100

    Refactor statement to check data was downloaded

--- src/api_request.py
+++ src/api_request.py
@@ -24,7 +24,7 @@ class ApiRequest:
 
     def run(self, api_params: Dict[str, Any]) -> pd.DataFrame:
         is_data = self._request(api_params)
-        if not is_data:
+        if is_data is None:
             return None
         downloaded_path = Path(api_params['out_dir']) / api_params['out_name']
         ds = xr.open_dataset(downloaded_path)
